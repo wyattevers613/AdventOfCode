5aed5b48b21f3155ee550e786bef1ca4e4190ad8
wyattevers613 <wyattevers613@gmail.com>
2020-12-02 14:02:28 -0500

    Day 2: Issue with Num 1

---
+++ Day 2 (python)/passwordPhilosophy.py
@@ -0,0 +1,39 @@
+# Day 2 Part A
+
+#create dictionary of allowed and actual
+
+with open("day2input.txt", "r") as readFile:
+    allowedActualDict = dict()
+    for line in readFile:
+        allowed, actual = line.strip().split(":")
+        allowedActualDict[allowed] = actual.strip()
+
+    
+# process keys and evaluate password validity
+
+validPasswords = 0
+
+for allowed in allowedActualDict.keys():
+    allRange, countChar = allowed.split(" ")
+    rangeStart, rangeEnd = allRange.split("-")
+    rangeStart = int(rangeStart)
+    rangeEnd = int(rangeEnd)
+    letterCount = 0
+
+    # print Str for debugging
+    printStr = f"Looking for: {countChar} in {allowedActualDict[allowed]}, {rangeStart} - {rangeEnd} times. "
+
+
+    for letter in allowedActualDict[allowed]:
+        if letter == countChar:
+            letterCount += 1
+
+    if rangeStart <= letterCount <= rangeEnd: 
+        validPasswords += 1
+        printStr += "Valid"
+    else:
+        printStr += "Invalid"
+    printStr += f". Found {letterCount} time(s)."
+    print(printStr)
+
+print(f"Found Solution: {validPasswords} are valid.")
